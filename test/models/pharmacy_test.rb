6e101515ed254c37c376553c881114aa4f1ec053
Penguin <say8425@gmail.com>
2017-05-12 22:52:33 +0900

    정보 API 접속 테스트 작성

--- test/models/pharmacy_test.rb
+++ test/models/pharmacy_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PharmacyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @url = 'http://openapi2.e-gen.or.kr/openapi/service/rest/ErmctInsttInfoInqireService/getParmacyBassInfoInqire'.freeze
+  end
+
+  test 'Connect Data go portal' do
+    params = { numOfRows: 1 }
+    connecting_portal = RestClient.get(@url, { accept: :json, params: params })
+    assert_equal connecting_portal.code, 200
+  end
 end
