58457b3a5e8191011fc9f2237dcdb70b975168ec
Penguin <say8425@gmail.com>
2017-05-29 02:44:45 +0900

    HTTParty를 기준으로 테스트 재작성

--- test/models/pharmacy_test.rb
+++ test/models/pharmacy_test.rb
@@ -2,12 +2,11 @@ require 'test_helper'
 
 class PharmacyTest < ActiveSupport::TestCase
   setup do
-    @url = 'http://openapi2.e-gen.or.kr/openapi/service/rest/ErmctInsttInfoInqireService/getParmacyBassInfoInqire'.freeze
+    @single = PharmacyPortal.new
   end
 
   test 'Connect Data go portal' do
-    params = { numOfRows: 1 }
-    connecting_portal = RestClient.get(@url, { accept: :json, params: params })
-    assert_equal connecting_portal.code, 200
+    get_infos = @single.infos
+    assert get_infos.success?, 'Accessing Data portal is FAIL!'
   end
 end
