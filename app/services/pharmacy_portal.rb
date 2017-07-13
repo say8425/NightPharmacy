b6bf2f4e7fbf6ecf4d420ee7addb7dec7eadf703
Penguin <say8425@gmail.com>
2017-07-13 18:41:00 +0900

    PharmacyPortal 서비스 코드 구조 변경, new에서 params 안받고 함수 호출할때 받게 함

--- app/services/pharmacy_portal.rb
+++ app/services/pharmacy_portal.rb
@@ -1,27 +1,28 @@
 class PharmacyPortal
-  attr_reader :options
+  attr_accessor :options
   include HTTParty
   base_uri 'openapi2.e-gen.or.kr/openapi/service/rest/ErmctInsttInfoInqireService'
 
-  def initialize(numOfRows=1, page=1)
+  def initialize
     @options = {
         headers: { Accept: 'application/json' },
-        numOfRows: numOfRows,
-        page: page,
-        ServiceKey: ENV['portal_secret_key']
+        query: {
+            pageNo: 1,
+            numOfRows: 1,
+            ServiceKey: ENV['portal_secret_key']
+        }
     }
   end
 
-  def infos
-    self.class.get('/getParmacyBassInfoInqire', options )
+  def infos(numOfRows)
+    options[:query][:numOfRows] = numOfRows
+
+    data = JSON.parse self.class.get('/getParmacyBassInfoInqire', options ).body, symbolize_names: true
+    data[:response][:body][:items][:item]
   end
 
-  def self.all_infos
-    options = {
-        headers: { Accept: 'application/json' },
-        numOfRows: 30,
-        ServiceKey: ENV['portal_secret_key']
-    }
+  def all_infos
+    options[:query][:numOfRows] = 50000
 
     data = JSON.parse get('/getParmacyBassInfoInqire', options ).body, symbolize_names: true
     data[:response][:body][:items][:item]
