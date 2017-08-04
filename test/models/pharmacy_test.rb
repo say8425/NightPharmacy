538ad9b1d72ccfa19b02b901821cca411ac8a493
Penguin <say8425@gmail.com>
2017-08-04 19:16:05 +0900

    pharmacy portal 테스트 코드 고침

--- test/models/pharmacy_test.rb
+++ test/models/pharmacy_test.rb
@@ -2,11 +2,22 @@ require 'test_helper'
 
 class PharmacyTest < ActiveSupport::TestCase
   setup do
-    @single = PharmacyPortal.new
+    @info = PharmacyPortal.new
   end
 
   test 'Connect Data go portal' do
-    get_infos = @single.infos
-    assert get_infos.success?, 'Accessing Data portal is FAIL!'
+    info = @info.connect_gov
+    assert info.success?, 'Accessing Data portal is FAIL!'
+  end
+
+  test 'Controlling Data length' do
+    info = @info.infos(2)
+    assert info.count.equal?(2), 'Controlling length of data is FAIL'
+
+    info = @info.infos(42)
+    assert info.count.equal?(42), 'Controlling length of data is FAIL'
+
+    info = @info.infos(15)
+    assert info.count.equal?(15), 'Controlling length of data is FAIL'
   end
 end
