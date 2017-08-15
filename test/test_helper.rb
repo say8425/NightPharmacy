2665de784d4642156dcf88a5a3d25c010ca4abb3
Penguin <say8425@gmail.com>
2017-08-15 15:03:20 +0900

    minitest 라이브러리 import 하고 report를 내보냄

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,14 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/ci'
 
 class ActiveSupport::TestCase
   include FactoryGirl::Syntax::Methods
+
+  if ENV['CIRCLE_TEST_REPORTS']
+    Minitest::Ci.report_dir = "#{ENV['CIRCLE_TEST_REPORTS']}/reports"
+  end
   # Add more helper methods to be used by all tests here...
 end
